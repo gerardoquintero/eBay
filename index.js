7c2010d27781bc6d4632cb53fb3b4c27fc3d7169
Bella Quintero <swanwolf18@gmail.com>
2024-02-28 18:38:46 -0700

    fixed app.join error

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const path = require("path")
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-app.set("views", app.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs")
 
+app.get('/', (req, res) => {
+    console.log("You're Home")
+})
 
-app.listen(8000, () => {
-    console.log("Connected to port 8000")
+app.listen(3000, () => {
+    console.log("Connected to port 3000")
 })
